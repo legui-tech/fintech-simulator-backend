c47f5a86a3b083e2092a55f441380cd91962d97c
Walter Leguiza <wal.leguiza@gmail.com>
2026-06-14 20:51:04 -0300

    FE-7 Initialize fintech-core-service with main application class and configuration

---
+++ fintech-core-service/src/main/java/ar/com/leguitech/fintechcoreservice/FintechCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.leguitech.fintechcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechCoreServiceApplication.class, args);
+    }
+
+}
